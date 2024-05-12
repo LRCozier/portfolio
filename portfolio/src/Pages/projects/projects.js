000a1e51cad54c275152cea061b5b8f0ea3eb5d6
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-05-12 18:16:01 +0100

    initial commit

---
+++ portfolio/src/Pages/projects/projects.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const about = () => {
+
+    return  (
+        <>
+        <h1>About me</h1>
+        <p> A motivated, self starting Frontend Developer; with a passion for creating
+            dynamic and engaging user interfaces.
+        </p>
+        <h2>Skills</h2>
+        <div className="skills-row">
+        </div>
+        </>
+        
+    )
+}
+
+export default about;

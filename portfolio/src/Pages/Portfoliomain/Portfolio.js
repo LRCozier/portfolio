b1c70078a4c851d9a63b9759c44c8fc1c166be32
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-05-28 09:10:33 +0100

    inital commit

---
+++ portfolio/src/Pages/Portfoliomain/Portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+import aboutpage from "../about/about";
+
+const Portfolio = () => (
+    <>
+        <aboutpage />
+    </>
+)
+
+export default Portfolio;
---
+++ portfolio/src/Pages/contact/contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ContactForm from '../../Components/contactform/contactform';
+
+const contact = () => {
+    <ContactForm/>
+}
+
+export default contact;
